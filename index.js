faa9522a1dc719076161087dca89b62f2444c46b
JasmineGWT <jazzgwwt@gmail.com>
2016-11-09 12:41:05 +1300

    Made shit work

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+var h = require('hyperscript')
 
 console.log('welcome to oh-shit-youre-dying!!!!!!')
+
+var test = h('h2', {}, 'HEEELLLLOOOOO!')
+
+var main = document.querySelector('main')
+
+main.appendChild(test)
