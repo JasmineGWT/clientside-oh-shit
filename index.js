f0bf46110228b1f0ec45b266313513c3a752091e
JasmineGWT <jazzgwwt@gmail.com>
2016-11-09 22:09:31 +1300

    Refactoring, kill me

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var main = document.querySelector('main')
 
 console.log('welcome to oh-shit-youre-dying!!!!!!')
 
-var title = h('h1', {}, "OH SHIT, YOU'RE DYING!")
+var smokerArray = [['nonSmoker', 'Not part of the #CancerCrew2K16'], ['smoker', '1-4'], ['smoker', '5-20'], ['posSmoker', 'Enough']]
+
+var title = h('div', {id: 'titleBack'}, h('h1', {}, "OH SHIT, YOU'RE DYING!"))
 main.appendChild(title)
 
 function doneLabels() {
@@ -26,6 +28,19 @@ function doneRadios() {
   return count
 }
 
+function getValueText(array) {
+  var radio = []
+  for (var i = 0; i <= 4; i++) {
+    console.log("swagggg", array[i][1])
+    var value = array[i][0]
+    var text = array[i][1]
+    radio.push(h('input', {type: 'radio'}, {name: 'q3'}, {value: value}, {id: 'q3_radio'}), h('lable', {for: value}, {id: 'q3_lable'}, text)
+    )
+    }
+    console.log(radio);
+  return radio
+}
+
 var form =
 h('form', {action:"/results"},{method:"post"},
 [
@@ -37,9 +52,12 @@ h('form', {action:"/results"},{method:"post"},
 
   h('h3', {class: 'questionTitle'}, 'On a scale of 1 to 10 how done are you?'),
     doneLabels(),
-    h('br'),
+      h('br'),
     doneRadios(),
-  h('h3', {class: 'questionTitle'}, 'How many durries do you smoke a day?')
+  h('h3', {class: 'questionTitle'}, 'How many durries do you smoke a day?'),
+  getValueText(smokerArray),
+    h('br'),
+  h('input', {type:'submit'}, {value: 'Bruh, when am I dying?'}, {id: 'submit'})
 ])
 
 main.appendChild(form)
