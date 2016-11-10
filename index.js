2dab98fe05e188976c25ffd7b83af5b9205651c4
JasmineGWT <jazzgwwt@gmail.com>
2016-11-10 19:56:50 +1300

    Did shit

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var h = require('hyperscript')
+var getValueTextOption = require('getValueTextOption')
 var main = document.querySelector('main')
 
 console.log('Welcome to Oh Shit, Youre Dying!!!!!!')
 
-var smokerArray = [['nonSmoker', 'Not part of the #CancerCrew2K16'], ['smoker', '1-4'], ['smoker', '5-20'], ['posSmoker', 'Enough']]
+var smokerArray = [
+    ['nonSmoker', 'Not part of the #CancerCrew2K16'],
+    ['smoker', '1-4'],
+    ['smoker', '5-20'],
+    ['posSmoker', 'Enough']
+]
+
+var drinkerArray = [
+  ['drinker', '1 shot'], ['drinker', '3 shots'], ['drinker', 'Bottle of vodka'], ['drinker', 'Alcohol poisioning is where I draw the line'], ['drinker', "If I could remember how much, it obviously wasn't enough"], ['bullshit', "I don't drink"]
+]
 
 var title = h('div', {id: 'titleBack'}, h('h1', {}, "OH SHIT, YOU'RE DYING!"))
 main.appendChild(title)
@@ -28,13 +38,13 @@ function doneRadios() {
   return count
 }
 
-function getValueText(array) {
+function getValueTextRadio(array) {
   var radio = []
-  for (var i = 0; i <= 4; i++) {
+  for (var i = 0; i < array.length; i++) {
     console.log("swagggg", array[i][1])
     var value = array[i][0]
     var text = array[i][1]
-    radio.push(h('input', {type: 'radio'}, {name: 'q3'}, {value: value}, {id: 'q3_radio'}), h('lable', {for: value}, {id: 'q3_lable'}, text)
+    radio.push(h('input', {type: 'radio'}, {name: 'q3'}, {value: value}, {id: 'q3_radio'}), h('lable', {for: value}, {id: 'q3_lable'}, text), h('br')
     )
     }
     console.log(radio);
@@ -54,9 +64,14 @@ h('form', {action:"/results"},{method:"post"},
     doneLabels(),
       h('br'),
     doneRadios(),
+
   h('h3', {class: 'questionTitle'}, 'How many durries do you smoke a day?'),
-  getValueText(smokerArray),
-    h('br'),
+    getValueTextRadio(smokerArray),
+
+  h('h3', {class: 'questionTitle'}, 'Cheeky Tuesday night drinks, how much do you drink?'),
+    getValueTextRadio(drinkerArray),
+      h('br'),
+
   h('input', {type:'submit'}, {value: 'Bruh, when am I dying?'}, {id: 'submit'})
 ])
 
--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
-var test = require('tape')
-
-test('testing oh-shit-youre-dying', function (t) {
-  t.ok(true)
-  t.end()
-})
+// var test = require('tape')
+//
+// test('testing oh-shit-youre-dying', function (t) {
+//   t.ok(true)
+//   t.end()
+// })
