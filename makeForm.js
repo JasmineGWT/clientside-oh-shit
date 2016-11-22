9ed15d87002d577e36376ff315f5c590b196e0d0
JasmineGWT <jazzgwwt@gmail.com>
2016-11-23 09:44:39 +1300

    Fixed indentation on makeForm.js

--- makeForm.js
+++ makeForm.js
@@ -15,19 +15,25 @@ module.exports = function makeForm(smokers, drinkers, countries) {
   [
     h('h3', {class: 'questionTitle'}, 'What is your name?'),
       h('input', {type: 'text'},{name: 'name'}),
+
     h('h3', {class: 'questionTitle'}, 'How old are you?'),
       h('input', {type: 'text'},{name: 'q1'}),
+
     h('h3', {class: 'questionTitle'}, 'Where are you from?'),
       h('select.whereAreYouFrom', {name: 'q3'}, whereAreYouFrom(countries)),
+
     h('h3', {class: 'questionTitle'}, 'On a scale of 1 to 10 how done are you?'),
       doneLabels(),
         h('br'),
       doneRadios(),
+
     h('h3', {class: 'questionTitle'}, 'How many durries do you smoke a day?'),
       getValueTextRadio(smokers),
+
     h('h3', {class: 'questionTitle'}, 'Cheeky Tuesday night drinks, how much do you drink?'),
       getValueTextRadio(drinkers),
         h('br'),
+        
     h('input', {type:'submit'}, {value: 'Bruh, when am I dying?'}, {id: 'submit'})
   ])
 }
